fe978909da5679ab1e44fd437bf48f2a0d8abedf
TonyChen91 <tonychen8382@gmail.com>
2023-07-24 18:38:55 +0800

    Update Qbank.py

--- Qbank.py
+++ Qbank.py
@@ -8,26 +8,30 @@ from docx.shared import Pt
 from docx.oxml.ns import qn
 from openpyxl import load_workbook
 ##待處理
-####刪除閱讀測驗
-####標題搜索
+####標題搜索連結加入
+####屬性excel(勾選單複數在excel > 查題目自動兩種都查)並且在doc有目錄
 ####超連結不知怎麼調標楷體
-####word開存關 開存關循環
-####如果題目標籤沒有XXX，則題目變成紅色
-####完成訊息增加有多少重複和多少長文
+####aminoisobutyric acid有BUG
+####如果沒有非主題，產生更多題目
 
-#可調控變數
-data="1116-組織"
-# data="1101-解剖"
-PATH = 'C:/Users/tony/Desktop/Qbank/chromedriver.exe'
+#---可調控變數---#
+data="頸部考訓-解剖"
+# data="1121-組織1"
+PATH = 'C:/Users/tony/Desktop/Qbank/chromedriver.exe' #可變變數地區路徑
 cooldown=1.0  #網速會影響
 
 
-#可變變數地區路徑
-Qrepeatcheck=list()
+#---程式變數一覽---#
+Qrepeatcheck=list() #檢測是否已經出現過這個題目
 keywords=list() #存單字庫
-checknum=0
-repeatnum=0
+checknum=0 #目前到第幾個單字
+repeatnum=0 #某單字有N個題目重複
+allquesnum=0 #某單字總共有N個題目
 allnum=0 #所有單字總數
+longquesnum=0 #某單字有N個長文題目
+invalidtagnum=0 #某單字有N個不合法標籤題目
+quetag=str() #某單字的標籤名稱
+valid_tags={'解剖','組織','生理','病理','醫學','生物','藥劑','藥物','治療','臨床','構造','醫務','醫師','視力','聽力','麻醉','復健','醫生','藥理','診斷','護理'}
 # baseurl="https://yamol.tw/tfulltext-"+keywords+".htm"
 
 option = webdriver.ChromeOptions()
@@ -38,7 +42,7 @@ option.add_experimental_option("detach", True)
 option.add_experimental_option("prefs", {"profile.password_manager_enabled": False, "credentials_enable_service": False})
 driver = webdriver.Chrome(executable_path=PATH, options=option)
 
-#讀取單字表
+#---讀取單字表---#
 wb = load_workbook('keywords.xlsx')
 sheet = wb[data]
 for k in range(1, sheet.max_row+1):
@@ -89,15 +93,18 @@ def valid_xml_char_ordinal(c):
 
 
 #創建word
-doc = docx.Document()
+doc_initial = docx.Document()
 # doc.add_heading('宥任自製題庫', level=1)
-doc.sections[0].header.paragraphs[0].text = "自動生成"+data+"題庫程式 by M110陳宥任。版權歸題目原作者所有。"
+doc_initial.sections[0].header.paragraphs[0].text = "自動生成"+data+"題庫程式 by M110陳宥任。版權歸題目原作者所有。"
+doc_initial.save(data+'.docx')
+doc = docx.Document(data+'.docx')
 
 #主程式 開始讀取
 def QbankStart(QbankStartUrl):
-    global checknum
-    global Qrepeatcheck
-    global repeatnum
+    global allquesnum, checknum, Qrepeatcheck, repeatnum, longquesnum, quetag, valid_tags, invalidtagnum
+    longquesnum = 0 #歸零
+    repeatnum = 0 #歸零
+    invalidtagnum = 0 #歸零
     driver.get(QbankStartUrl)
     #
     time.sleep(cooldown)
@@ -107,6 +114,7 @@ def QbankStart(QbankStartUrl):
     Qtexts = driver.find_elements(By.CLASS_NAME, 'itemcontent')
     Anstexts = driver.find_elements(By.XPATH, '//*[@class="alert alert-success"]/div/div[1]/b[1]/a')
     Links = [Anstext.get_attribute('href') for Anstext in Anstexts]
+    Qtags = driver.find_elements(By.XPATH, '//*[@class="col-lg-12 reponse-card"]/div[2]/div[@style="text-align:right"]')
     # for Qtext in Qtexts:
     #     print(Qtext.text)
     # for Anstext in Anstexts:
@@ -114,33 +122,46 @@ def QbankStart(QbankStartUrl):
     if(len(Qtexts)!=len(Anstexts)):
         print("題目與答案數目不一致!!!")
         driver.quit()
+    allquesnum = len(Qtexts)
     for Qtext in Qtexts:
-        if Links[Qtexts.index(Qtext)] not in Qrepeatcheck:
-            if len(str(Qtext.text)) < 1000:
+        tagbool=False #標籤判斷是否合法 預設無
+        if Links[Qtexts.index(Qtext)] in Qrepeatcheck:
+            # print("已重複")
+            repeatnum+=1
+            # doc.add_paragraph("已重複")
+        elif len(str(Qtext.text)) > 1000:
+            longquesnum+=1
+            # add_hyperlink(doc.add_paragraph(), Links[Qtexts.index(Qtext)], "自動刪除長文", None, True)
+            # doc.add_paragraph("-----")
+        else:
+            #-----#
+            quetag = ''.join(c for c in str(Qtags[Qtexts.index(Qtext)].text) if valid_xml_char_ordinal(c))
+            for tag in valid_tags:
+                if tag in quetag: tagbool=True
+            if tagbool:
                 Qrepeatcheck.append(Links[Qtexts.index(Qtext)])
                 doc.add_paragraph(''.join(c for c in str(Qtext.text) if valid_xml_char_ordinal(c)))
                 add_hyperlink(doc.add_paragraph(), Links[Qtexts.index(Qtext)], "答案: "+Anstexts[Qtexts.index(Qtext)].text+"(解析連結)", None, True)
                 doc.add_paragraph("-----")
-            else:
-                add_hyperlink(doc.add_paragraph(), Links[Qtexts.index(Qtext)], "自動刪除長文", None, True)
-                doc.add_paragraph("-----")
-            # Qrepeatcheck.append(Links[Qtexts.index(Qtext)])
-            # doc.add_paragraph(''.join(c for c in str(Qtext.text) if valid_xml_char_ordinal(c)))
-            # add_hyperlink(doc.add_paragraph(), Links[Qtexts.index(Qtext)], "答案: "+Anstexts[Qtexts.index(Qtext)].text+"(解析連結)", None, True)
-            # doc.add_paragraph("-----")
-        else:
-            # print("已重複")
-            repeatnum+=1
-            # doc.add_paragraph("已重複")
-        # doc.add_paragraph("-----")
+                # print("符合標籤")
+            else: 
+                invalidtagnum +=1
+                print("!!!已刪除-----> "+quetag)
+            #-----#                    
     checknum+=1
     
 
 for kw in keywords:
+    if int(allnum) > 1 and checknum == int(allnum)//2:
+        doc.save(data+'.docx')
+        print("!!!--------進度 50% 存檔--------!!!")
+        time.sleep(1.0)
+        doc = docx.Document(data+'.docx')
     QbankStart("https://yamol.tw/tfulltext-"+str(kw)+".htm")
     time.sleep(0.3)
-    print("完成"+kw+" 進度:"+str(checknum)+"/"+str(allnum))
+    print("完成"+kw+" 進度:"+str(checknum)+"/"+str(allnum)+" 一共"+str(allquesnum)+"題: "+str(repeatnum)+"重複,"+str(longquesnum)+"長文,"+str(invalidtagnum)+"非主題")
 
+#全部標楷體化
 # for paragraph in doc.paragraphs:
 #     # if paragraph.style.name.startswith('Heading'):
 #     for run in paragraph.runs:
